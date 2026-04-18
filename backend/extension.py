f859632541d52ebeeb89421c1b3dcdcb52d1ecc6
Shivang tenguriya <shivangtenguriya9897@gmail.com>
2026-04-18 22:29:27 +0530

    Redis Fixed

--- backend/extension.py
+++ backend/extension.py
@@ -4,6 +4,6 @@ from flask_limiter.util import get_remote_address
 limiter = Limiter(
     key_func=get_remote_address,
     strategy="fixed-window",
-    storage_uri="redis://localhost:6379",
+    # storage_uri="redis://localhost:6379",
     default_limits=["200 per day", "50 per hour"]
 )
